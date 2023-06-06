0284376d404099a680011ad53e8036f129e773fe
Alaa Salah <40337389+progalaa99@users.noreply.github.com>
2023-06-06 23:34:47 +0300

    Update index.blade.php

--- ba/resources/views/doctorpresent/index.blade.php
+++ ba/resources/views/doctorpresent/index.blade.php
@@ -1,1 +1,8 @@
-<h1> {{$days}} </h1>
+<h1> {{ $days }} </h1>
+<h1>{{ $start_time++ }}</h1>
+<form method="POST" action="/save-time">
+    @csrf
+    <label for="time">Enter Time:</label>
+    <input type="time" name="time" id="time" required>
+    <button type="submit">Save</button>
+</form>
