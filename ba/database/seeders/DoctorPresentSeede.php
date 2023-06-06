05fd109c0de8577797ec9978c4ad68d0dc79307d
Alaa Salah <40337389+progalaa99@users.noreply.github.com>
2023-06-06 22:20:40 +0300

    up

---
+++ ba/database/seeders/DoctorPresentSeede.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DoctorPresentSeede extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('ifdoctorpresents')->insert([
+            [
+                'days' => 'Monday',
+                'start_time' => '16:00:00',
+                'end_time' => '19:00:00',
+            ],
+            [
+                'days' => 'Tuesday',
+                'start_time' => '16:00:00',
+                'end_time' => '19:00:00',
+            ],
+            [
+                'days' => 'Tuesday',
+                'start_time' => '16:00:00',
+                'end_time' => '19:00:00',
+            ],
+            [
+                'days' => 'Thursday',
+                'start_time' => '16:00:00',
+                'end_time' => '19:00:00',
+            ],
+           
+           
+        ]);
+    }
+    
+}
